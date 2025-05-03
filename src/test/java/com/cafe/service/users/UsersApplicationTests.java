62ddd6662d9ae6e891a8f5a5c6ac2db431f9ebf8
Rohit Dubey <dubey_rohit@outlook.com>
2025-05-03 18:38:17 +0530

    jenkins file update

--- src/test/java/com/cafe/service/users/UsersApplicationTests.java
+++ src/test/java/com/cafe/service/users/UsersApplicationTests.java
@@ -2,9 +2,15 @@ package com.cafe.service.users;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
+
 class UsersApplicationTests {
+
+	@MockitoBean
+	private JwtDecoder jwtDecoder;
 
 	@Test
 	void contextLoads() {
